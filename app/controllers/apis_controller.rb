1137d387f7d8f60e9dc4199a420eb8d41b6ed0fe
Ash Blue <ash@blueashes.com>
2015-10-24 22:48:56 -0700

    Devise now accepts AJAX requests

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,10 +1,6 @@
 class ApisController < ApplicationController
   before_filter :verify_user
 
-  # @TODO Testing only, remove before production
-  # Will require that we place the token in the body and push it for requests
-  skip_before_action :verify_authenticity_token
-
   private
   def verify_user
     unless user_signed_in?
