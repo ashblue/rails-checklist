0bd393b162bd4c5d4f98708ad5063d53e4476c61
Ash Blue <ash@blueashes.com>
2015-10-25 22:27:42 -0700

    JSON requests now pre-process data properly

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,8 +1,12 @@
 class ApisController < ApplicationController
-  before_filter :authenticate_user_from_token!
+  before_filter :authenticate_user_from_token!, :parse_request
 
   private
+
+  # Prevent Rails meta data and params from leaking in by only passing the request body
   def parse_request
-    @json = JSON.parse(request.body.read.html_safe)
+    unless request.body.read.empty?
+      @json = JSON.parse(request.body.read.html_safe)
+    end
   end
 end
