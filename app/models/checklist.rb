4e2d6d587fe7ea198ce4ad54ad9be1b22818948a
Ash Blue <ash@blueashes.com>
2015-10-25 22:30:49 -0700

    Checklists now auto-process JSON properly for array references

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -7,4 +7,11 @@ class Checklist
   key :title, String
   key :created_at, Time, :default => Time.now
   key :updated_at, Time, :default => Time.now
+
+  def as_json(options = {})
+    @json = super(options)
+    @json[:entries] = self.entries.map { |e| e.id }
+
+    @json
+  end
 end
